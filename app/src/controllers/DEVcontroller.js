02478a6e97880d0ca7652e1ca6ed41647f78aaaf
Kesley David <kesleygti@gmail.com>
2019-08-13 00:19:34 -0300

    app => [dev] [new] Controller Store
    Controller Add Dev

--- app/src/controllers/DEVcontroller.js
+++ app/src/controllers/DEVcontroller.js
@@ -1,5 +1,30 @@
+const axios = require('axios');
+const DEV = require('../models/DEV');
+
 module.exports = {
-  store(req, res) {
-    return res.json({ok:true});
+  async store(req, res) {
+    const { username } = req.body;
+
+    // Verifica se usuário já está cadastrado
+    // Se tiver, retorna seus dados
+    const userExists = await DEV.findOne({ devUser:username });
+    if (userExists) {
+      return res.json(userExists);
+    }
+
+    // Consultar API GitHub
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+
+    // Alterar "avatar_url" para "devAvatar"
+    const { name:devName, bio:devBio, avatar_url:devAvatar } = response.data;
+
+    const dev = await DEV.create({
+      devName,
+      devUser:username,
+      devBio,
+      devAvatar
+    });
+
+    return res.json(dev);
   }
 }
