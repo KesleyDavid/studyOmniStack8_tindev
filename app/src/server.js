230164f548638b1d5ef226dc02d75125f55a7d54
Kesley David <kesleygti@gmail.com>
2019-08-12 23:42:56 -0300

    app => [dev] [new] [db] Getting started with MongoDb
    Connection OK

--- app/src/server.js
+++ app/src/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
 
+// mongodb+srv://kdfsoftware:<password>@cluster0-7wpct.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://kdfsoftware:kdfSoftware@cluster0-7wpct.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+});
+
 // Avisar Express que os dados serão
 server.use(express.json());
-
 server.use(routes);
 
 server.listen(3333);
