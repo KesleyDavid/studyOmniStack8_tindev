160799d84491e82eb863c60a698443ea31cce7f6
Kesley David <kesleygti@gmail.com>
2019-08-18 23:07:00 -0300

    web => [dev] [new] Page Login

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Login.css'; 
+// Login.css -> Quando é só um arquivo, não precisa dar nome
 
 import logo from '../assets/logo.svg';
 
 export default function Login(){
   return (
-    <img src={logo} alt="Tindev"/>
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Tindev"/>
+        <input placeholder="Digite seu usuário no Github"/>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   )
 }
